0b13c35acf57da2c49520db51cab22d207a260e1
Big Kent <s3863969@rmit.edu.vn>
2021-05-06 16:30:34 +0700

    Kent

--- Pages/BareBearsJavascript.js
+++ Pages/BareBearsJavascript.js
@@ -47,21 +47,3 @@ function myFunction(x) {
   var x = window.matchMedia("(min-width: 1050px)");
   myFunction(x) ;
   x.addEventListener(myFunction)
-
-//javascript of new05 adding by Kent//
-  function inputBox() {
-   var password= new RegExp (/^[password]$/)
-   if (password.test(Password)) {
-      return true
-   }
-   else {
-      alert("Incorrect Password or Username")
-   }
-   var emailUser= new RegExp (/^[A-Za-z0-9_.]{2,100}+@[A-Za-z0-9.-]{2,100}+\.[A-Za-z]{2,100}$/)
-   if (emailUser.test(Email)) {
-      return true
-   }
-   else {
-      alert("Incorrect Password or Username")
-   }
-}

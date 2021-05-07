19302cc49394efa8b19386712ee0ffed8340b520
Kultras Hub <s3863956@rmit.edu.vn>
2021-05-07 16:17:16 +0700

    Cookie applied to all pages

--- Pages/JS/Cookies.js
+++ Pages/JS/Cookies.js
@@ -1,36 +1,64 @@
+
 function CreateCookie(name, value, days) {
     var date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    var expires = "; expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + expires + "; path=/";
+    var expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value +";"+ expires + ";path=/";
 }
-function readCookie(cname) {
+function ReadCookie(cname) {
     var name = cname + "=";
-    var decodedCookie=decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
+    var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' '){ 
-            c = c.substring(1, c.length);
+            c = c.substring(1);
         }
         if (c.indexOf(name) == 0){ 
             return c.substring(name.length, c.length)
         };
     }
-    return null;
+    return "";
 }
 
 function eraseCookie(name) {
     createCookie(name, "", -1);
 }
-function checkCookie()
+function CheckCookie()
 {
-    var user =getCookie("username");
-    if(user!="")
+    var user =ReadCookie("username");
+    
+
+    if(user!=""&&user!=null)
     {
         //do nothing
+        console.log('cookies found');
     }
     else{
         //ask for a cookie
+        console.log('cookies unfound'+user);
+        ShowCookie();
     }
 }
+function ShowCookie()
+{
+    var cookiesMenu=document.getElementById("cookies");
+    //open Cookies
+    cookiesMenu.style.display="flex";
+    cookiesMenu.style.bottom="0";
+}
+//NOTICE
+//Hide Cookie should be in Create Cookie function but not Create Cookie is called in HideCookie()
+//becase HideCookie() is the one get called on cookie menu
+//not fixed;
+function HideCookie()
+{
+    user = prompt("Please enter your name:","");
+    CreateCookie("username",user,7);
+    var cookiesMenu=document.getElementById("cookies");
+    //open Cookies
+    cookiesMenu.style.display="none";
+    cookiesMenu.style.bottom="-40px";
+}
+setTimeout(function(){
+    CheckCookie();
+},5000);

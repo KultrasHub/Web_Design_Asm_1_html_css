4dfe74e311fd9e3147afee681c275a99b05ddbde
Quoc Phong <s3803566@rmit.edu.vn>
2021-05-07 23:08:44 +0700

    this is very weird

--- Pages/JS/Register.js
+++ Pages/JS/Register.js
@@ -12,6 +12,13 @@ function infoFunc(){
 
   var zipCheck=/[0-9](?=.{4,6})/;
 
+  var elem;
+
+  //Create warning message
+  elem=document.creatElement('div');
+  elem.id="notify";
+  elem.style.display="none";
+
   //values
   var emailValue = document.getElementById("emailVeri").value;
 
@@ -33,7 +40,11 @@ function infoFunc(){
 
   //Conditions
   if(!emailCheck.test(emailValue)){
-    alert("Please recheck your email as is it not in the correct format.");
+    inputBox[0].appendChild(elem);
+    emailValue.className='invalid';
+    elem.textContent="Wrong email format.";
+    elem.className='error';
+    elem.style.display='block';
     return false;
   }
 
@@ -52,18 +63,23 @@ function infoFunc(){
     return false;
   }
 
-  if(allCheck.test(firstValue) == false){
+  if(!allCheck.test(firstValue)){
     alert("First name needs to be 3 characters or more.");
-  } else if (allCheck.test(lastValue) == false) {
+    return false;
+  } else if (!allCheck.test(lastValue)) {
     alert("Last name needs to be 3 characters or more.");
-  } else if (allCheck.test(cityValue) == false) {
+    return false;
+  } else if (!allCheck.test(cityValue)) {
     alert("City needs to be 3 characters or more.");
-  } else if (allCheck.test(addressValue) == false) {
+    return false;
+  } else if (!allCheck.test(addressValue)) {
     alert("Address needs to be 3 characters or more.");
+    return false;
   }
 
-  if(zipCheck.test(zipValue) == false){
+  if(!zipCheck.test(zipValue)){
     alert("Zip needs to contain between 4-6 digits.");
+    return false;
   }
   return true;
 }

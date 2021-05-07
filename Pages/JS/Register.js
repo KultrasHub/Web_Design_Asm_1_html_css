579f38b4048f6a56e76eb61186aac4be26f97406
Quoc Phong <s3803566@rmit.edu.vn>
2021-05-07 19:55:32 +0700

    Rearranging

--- Pages/JS/Register.js
+++ Pages/JS/Register.js
@@ -1,12 +1,35 @@
 
 function infoFunc(){
-  //var for email
+
+  //var for checking
   var emailCheck =/^(([a-zA-Z0-9]+[.]?)+[a-zA-Z0-9]*){3,}@(([a-zA-Z0-9]+[.]?)*[a-zA-Z0-9]+)+([.]{1}[a-zA-Z0-9]{2,})+$/;
 
+  var phoneCheck =/^([0-9][-.]?){9,11}$/;
 
+  var passCheck =/^(?=.{8,20})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/;
 
-  //values for email
+  var allCheck =/(?=.{3,})/;
+
+  var zipCheck=/[0-9](?=.{4,6})/;
+
+  //values
   var emailValue = document.getElementById("emailVeri").value;
+
+  var phoneValue = document.getElementById("phoneVeri").value;
+
+  var passValue = document.getElementById("passVeri").value;
+
+  var rePassValue = document.getElementById("rePassVeri").value;
+
+  var firstValue = document.getElementById("firstVeri").value;
+
+  var lastValue = document.getElementById("lastVeri").value;
+
+  var cityValue = document.getElementById("cityVeri").value;
+
+  var addressValue = document.getElementById("addressVeri").value;
+
+  var zipValue = document.getElementById("zipVeri").value;
 
   //Conditions
   if(!emailCheck.test(emailValue)){
@@ -14,25 +37,11 @@ function infoFunc(){
     return false;
   }
 
-  //var for phone
-  var phoneCheck =/^([0-9][-.]?){9,11}$/;
-
-  //values for phone
-  var phoneValue = document.getElementById("phoneVeri").value;
-
-  //Conditions
   if(!phoneCheck.test(phoneValue)){
     alert("Please recheck your phone as is it not in the correct format.");
     return false;
   }
-  //Condition for password
-  var passCheck =/^(?=.{8,20})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/;
 
-  //Getting values for password/retype inputs
-  var passValue = document.getElementById("passVeri").value;
-  var rePassValue = document.getElementById("rePassVeri").value;
-
-  //conditions for the password/retype field
   if(passCheck.test(passValue) == true){
     if(rePassValue != passValue){
 			alert("You retyped it wrong.");
@@ -43,19 +52,6 @@ function infoFunc(){
     return false;
 	}
 
-	//testing/debugging command: alert(passValue + ":" + passResult);
-
-
-  //Moving on to first/last name, address and city
-  var allCheck =/(?=.{3,})/;
-
-  //Getting values for each part
-  var firstValue = document.getElementById("firstVeri").value;
-  var lastValue = document.getElementById("lastVeri").value;
-  var cityValue = document.getElementById("cityVeri").value;
-  var addressValue = document.getElementById("addressVeri").value;
-
-  //Condition for the info above (I made it detailly to let user know where they got it wrong)
   if(allCheck.test(firstValue) == false){
     alert("First name needs to be 3 characters or more.");
   }else if(allCheck.test(lastValue) == false){
@@ -65,16 +61,7 @@ function infoFunc(){
   }else if(allCheck.test(addressValueValue) == false){
     alert("Address needs to be 3 characters or more.");
   }
-
-  //Zip value
-  var zipCheck=/[0-9](?=.{4,6})/;
-
-  //Getting Values for Zipcode
-  var zipValue = document.getElementById("zipVeri").value;
-
-  //checking alert(zipValue + ": " + zipCheck.test(zipValue));
-
-  //Condition and alerting
+  
   if(zipCheck.test(zipValue) == false){
     alert("Zip needs to contain between 4-6 digits.");
   }

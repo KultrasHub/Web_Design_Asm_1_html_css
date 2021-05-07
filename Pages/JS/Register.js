a6fed700cb073f8b2dd2ca9827700c56ee9f699c
Quoc Phong <s3803566@rmit.edu.vn>
2021-05-07 11:37:22 +0700

    Adding other input section, zip is buggy though, currently working on how to prevent the page to continue when the information inputted is incorrect.

--- Pages/JS/Register.js
+++ Pages/JS/Register.js
@@ -11,15 +11,47 @@ function infoFunc(){
 
   //conditions for the password/retype field
   if(passCheck.test(passValue) == true){
-		if(rePassValue == passValue){
-			alert("congrats");
-		}else{
-			alert("you retyped it wrong");
+		if(rePassValue != passValue){
+			alert("You retyped it wrong.");
 		}
 
 	}else{
-		alert("You did not meet the requirements for the password, please try again");
+		alert("The password requires 8-20 characters including an uppercase, lowercase, atleast one special character and digit.");
 	}
 
 	//testing/debugging command: alert(passValue + ":" + passResult);
+
+
+  //Moving on to first/last name, address and city
+  var allCheck =/(?=.{3,})/;
+
+  //Getting values for each part
+  var firstValue = document.getElementById("firstVeri").value;
+  var lastValue = document.getElementById("lastVeri").value;
+  var cityValue = document.getElementById("cityVeri").value;
+  var addressValue = document.getElementById("addressVeri").value;
+
+  //Condition for the info above (I made it detailly to let user know where they got it wrong)
+  if(allCheck.test(firstValue) == false){
+    alert("First name needs to be 3 characters or more.");
+  }else if(allCheck.test(lastValue) == false){
+    alert("Last name needs to be 3 characters or more.");
+  }else if(allCheck.test(cityValue) == false){
+    alert("City needs to be 3 characters or more.");
+  }else if(allCheck.test(addressValueValue) == false){
+    alert("Address needs to be 3 characters or more.");
+  }
+
+  //Zip value
+  var zipCheck=/(?=.{4,6})/;
+
+  //Getting Values for Zipcode
+  var zipValue = document.getElementById("zipVeri").value;
+
+  //checking alert(zipValue + ": " + zipCheck.test(zipValue));
+
+  //Condition and alerting
+  if(zipCheck.test(zipValue) == false){
+    alert("Zip needs to contain between 4-6 digits.");
+  }
 }

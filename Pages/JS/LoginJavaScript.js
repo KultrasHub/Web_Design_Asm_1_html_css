d914adeed53e687e7bb4eb2e98e3570f1127564f
Big Kent <s3863969@rmit.edu.vn>
2021-05-07 16:19:35 +0700

    Kent test new 05 n time :))

--- Pages/JS/LoginJavaScript.js
+++ Pages/JS/LoginJavaScript.js
@@ -16,20 +16,32 @@
 //       alert("Incorrect Password or Username");
 //    }
 // }
-function validateEmail(password) {
- var psword = /^password$/;
- if (password.match(psword)) {
-    return true;s
- }
- else {
-    alert("Incorrect Password or Username");
- }}
-function validateEmail(email) {
- var emailUser= new RegExp (/?!^[.+&'_-]*@.*$)(^[_\w\d+&'-]+(\.[_\w\d+&'-]*)*@[\w\d-]+(\.[\w\d-]+)*\.(([\d]{1,3})|([\w]{2,}))$/);
- if (emailUser.test(User Email)) {
-    return true;
- }
- else {
-    alert("Incorrect Password or Username");
- }
-}
+// function validateEmail(password) {
+//  var psword = /^password$/;
+//  if (password.match(psword)) {
+//     return true;s
+//  }
+//  else {
+//     alert("Incorrect Password or Username");
+//  }}
+// function validateEmail(email) {
+//  var emailUser= new RegExp (/?!^[.+&'_-]*@.*$)(^[_\w\d+&'-]+(\.[_\w\d+&'-]*)*@[\w\d-]+(\.[\w\d-]+)*\.(([\d]{1,3})|([\w]{2,}))$/);
+//  if (emailUser.test(User Email)) {
+//     return true;
+//  }
+//  else {
+//     alert("Incorrect Password or Username");
+//  }
+// }
+function validateEmail(){
+  var password=("^password$/");
+  var email=/?!^[.+&'_-]*@.*$)(^[_\w\d+&'-]+(\.[_\w\d+&'-]*)*@[\w\d-]+(\.[\w\d-]+)*\.(([\d]{1,3})|([\w]{2,}))$/;
+  var psword = document.getElementById("pswordLogin").value;
+  var uemail = document.getElementById("emailLogin").value;
+  if (password.values.match(psword) == True) {
+    return True;}
+    else if (password != psword) {
+      alert("Wrong password, please try again.");
+      return false;
+      reload(true);}
+    }

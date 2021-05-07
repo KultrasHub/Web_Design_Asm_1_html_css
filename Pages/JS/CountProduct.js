43f61d179975bd8537ded2f553e0153bb5a63873
Big Kent <s3863969@rmit.edu.vn>
2021-05-07 21:00:28 +0700

    Kent finish new 07 please check

--- Pages/JS/CountProduct.js
+++ Pages/JS/CountProduct.js
@@ -1,4 +1,4 @@
-let carts = document.querySelectorAll('.proButt');
+let carts = document.querySelectorAll('.cartButt');
 
 let products = [
     {
@@ -14,9 +14,9 @@ let products = [
         inCart: 0
     },
     {
-        name:'Cute Emotional Octopus',
-        tag:'octo',
-        price: 10.99,
+        name:'Your Name',
+        tag:'YN',
+        price: 4.49,
         inCart: 0
     },
 ]
@@ -27,8 +27,22 @@ for (let i=0; i<carts.length; i++){
   })
 }
 
+function onLoadCartNumbers(){
+  let productNumbers = localStorage.getItem('cartNumbers');
+  if (productNumbers) {
+    document.querySelector('.cart span').textContent = productNumbers;
+  }
+}
+
 function cartNumbers() {
     let productNumbers = localStorage.getItem('cartNumbers');
     productNumbers = parseInt(productNumbers);
-    localStorage.setItem('cartNumbers', 1);
+    if (productNumbers) {
+      localStorage.setItem('cartNumbers', productNumbers + 1);
+      document.querySelector('.cart span').textContent = 1;
+    } else {
+      localStorage.setItem('cartNumbers', 1);
+      document.querySelector('.cart span').textContent = 1;
+    }
 }
+onLoadCartNumbers();

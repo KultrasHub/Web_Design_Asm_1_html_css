b14deec4cdf10e4fe916882355b82859dd05a3fa
Kultras Hub <s3863956@rmit.edu.vn>
2021-05-08 16:43:31 +0700

    Fix the bug when remove button erase all products

--- Pages/JS/OrderPage.js
+++ Pages/JS/OrderPage.js
@@ -1,6 +1,7 @@
 //defaultValue
 var defaultSubTotal = 0;
 var defaultShipping = 2;
+var currentVoucher = 0;
 //voucher
 var vouchers = [
     {
@@ -18,9 +19,10 @@ function CalculateInvoice(subtotal, shipping, voucher) {
     let shippingBox = document.getElementById("shipping");
     let voucherBox = document.getElementById("voucherBox");
     let totalBox = document.getElementById("totalBox");
-    subtotalBox.innerHTML = subtotal.toString();
+    let subtotalFloat = parseFloat(subtotal).toFixed(2);
+    subtotalBox.innerHTML = subtotalFloat.toString();
     shippingBox.innerHTML = shipping.toString();
-    let product = subtotal * voucher;
+    let product = subtotalFloat * voucher;
     let result = product / 100;
     let voucherAmount = result.toFixed(2);
     let totalAmount = parseFloat(subtotal) + parseFloat(shipping) - parseFloat(voucherAmount);
@@ -44,6 +46,7 @@ function ApplyVoucher() {
         return 0;
     }
     let finalValue = CheckVoucher(voucherBoxvalue.value);
+    currentVoucher = finalValue;
     if (finalValue == 0) {
         //display
         let discountAmount = document.getElementById("discountAmount");
@@ -90,14 +93,14 @@ function Analyze() {
         for (let i = 0; i < closeList.length; i++) {
             //console.log("oncheck:"+x+" "+i);
             if (x == closeList[i]) {
-                
+
                 return false;
             }
         }
         //console.log("passed:"+x);
         return true;
     }
-    let passedBox=1;//reduce Box
+    let passedBox = 1;//reduce Box
     for (let i = 0; i < productData.length; i++) {
         if (BeNotInCloseList(i)) {
             let proAmount = parseInt(productData[i][6]);
@@ -108,6 +111,7 @@ function Analyze() {
                     proAmount += parseInt(productData[j][6]);
                     closeList.push(j);
                     //delete on storage
+                    console.log("compare:" + i + "deleted" + j);
                     localStorage.removeItem(productData[j][7]);
                     //also change count
                     let currentcount = localStorage.getItem("Count");
@@ -118,17 +122,18 @@ function Analyze() {
             }
 
             //checking i complete
-            //add box here
-        
+            //add box here      
             AddBox(productData[i][0], productData[i][2], productData[i][3], productData[i][4], productData[i][5], proAmount, productData[i][7]);
+            //remove the current product in shop and save it to the new position
+            localStorage.removeItem(productData[i][7]);
             //save to storage
-            let stringValue=productData[i][0]+";"+productData[i][1]+";"+productData[i][2]+";"+productData[i][3]+";"+productData[i][4]+";"+productData[i][5]+";"+proAmount.toString();
-            localStorage.setItem("productNo."+passedBox.toString(),stringValue);
-            passedBox+=1;
+            let stringValue = productData[i][0] + ";" + productData[i][1] + ";" + productData[i][2] + ";" + productData[i][3] + ";" + productData[i][4] + ";" + productData[i][5] + ";" + proAmount.toString();
+            localStorage.setItem("productNo." + passedBox.toString(), stringValue);
+            passedBox += 1;
         }
     }
-    let mainAmount=document.getElementById("MainAmount");
-    mainAmount.innerHTML=(passedBox-1).toString()+" items";
+    let mainAmount = document.getElementById("MainAmount");
+    mainAmount.innerHTML = (passedBox - 1).toString() + " items";
 
 }
 function AddBox(name, author, price, photo, alt, amount, productKey) {
@@ -159,6 +164,11 @@ function AddBox(name, author, price, photo, alt, amount, productKey) {
     pAmount.value = amount;
     pAmount.required = 'required';
     left.appendChild(pAmount);
+    //left-inputSecret
+    let pinputSecret = document.createElement('span');
+    pinputSecret.style.display = 'none';
+    pinputSecret.innerHTML = price;
+    pAmount.appendChild(pinputSecret);
     //right
     let right = document.createElement('div');
     right.className = "right";
@@ -166,12 +176,14 @@ function AddBox(name, author, price, photo, alt, amount, productKey) {
     //right-prices
     let pprice = document.createElement('span');
     pprice.className = "prices";
+    pprice.id = "prices";
     //calculate the product of price and amount
     let totalPrice = parseFloat(price) * parseInt(amount);
     totalPrice = totalPrice.toFixed(2);
     //add total price to subtotal
-    defaultSubTotal+=parseFloat(totalPrice);
-    CalculateInvoice(defaultSubTotal,defaultShipping,0);
+    defaultSubTotal += parseFloat(totalPrice);
+    defaultSubTotal.toFixed(2);
+    CalculateInvoice(defaultSubTotal, defaultShipping, 0);
     pprice.innerHTML = totalPrice;
     right.appendChild(pprice);
     //right-remove
@@ -186,14 +198,57 @@ function AddBox(name, author, price, photo, alt, amount, productKey) {
     pHidden.innerHTML = productKey;
     pHidden.style.display = 'none';
     premove.appendChild(pHidden);
+    //add input on input
+    pAmount.oninput = function () {
+        UpdateAmount(this, pprice);
+    }
+
+}
+function UpdateAmount(o, pBox) {
+    //get the hidden price
+    let hiddenPrice = o.children[0].innerHTML;
+    let price = parseFloat(hiddenPrice);
+    let value = parseInt(o.value);
+    if (value != null && value != "" &&o.value.length!=0) {
+        //compute the total price
+        let result = price * parseInt(o.value);
+        result = parseFloat(result).toFixed(2);
+        //get the value and subtract it from subtotal
+        let currentValue = parseFloat(pBox.innerHTML);
+        defaultSubTotal =parseFloat(defaultSubTotal)- currentValue;
+        defaultSubTotal += parseFloat(result);
+        CalculateInvoice(defaultSubTotal, defaultShipping, currentVoucher);
+        //display on the pbox
+        pBox.innerHTML = result.toString();
+    }
+    else {
+        let currentValue = parseFloat(pBox.innerHTML);
+        defaultSubTotal-=parseFloat(currentValue);
+        CalculateInvoice(defaultSubTotal, defaultShipping, currentVoucher);
+        pBox.innerHTML = '0';
+    }
+    //update 
 }
 function DeleteBlock(o) {
     let hiddenData = o.children[0].innerHTML;
+    let num = parseInt(hiddenData.match(/\d+/g));
+
     if (hiddenData != null) {
-        localStorage.removeItem(hiddenData);
-        //also change count
+        let code = 'productNo.';
+        //console.log("deleted:"+num);
         let currentcount = localStorage.getItem("Count");
         let currentcountValue = parseInt(currentcount);
+        localStorage.removeItem(code + num.toString());
+        //replace the n with n+1
+        //first get item productNo.n+1 and save it as productNo.n
+        for (var i = num + 1; i <= currentcountValue; i++) {
+            let product = localStorage.getItem(code + i.toString());
+            let smaller = i - 1;
+            localStorage.setItem(code + smaller.toString(), product);
+            //delete the old one
+            localStorage.removeItem(code + i.toString());
+        }
+        //also change count
         currentcountValue -= 1;
         localStorage.setItem("Count", currentcountValue.toString());
         location.reload();

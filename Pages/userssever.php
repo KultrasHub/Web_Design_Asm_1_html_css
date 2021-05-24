ef4214d834f302bd780777d0db03538345477273
Big Kent <s3863969@rmit.edu.vn>
2021-05-24 20:05:32 +0700

    new PHP5 code

--- Pages/userssever.php
+++ Pages/userssever.php
@@ -1,95 +1,49 @@
 <?php
 session_start();
 
-//call all the infor we need to store//
-$phonenum="";
-$email="";
-$username="";
-$password="";
-$error=array();
+//call all element//
+$error = array();
+$username = "";
+$email = "";
+$phonenumber = "";
+$password = "";
+$cpassword = "";
 
-//conect to database in XAMPP//
-$database = mysqli_connect('localhost','root','','userinfor') or die("Could not connect to database.");
+//element represent for//
+$username = $_POST['firstName'];
+$phonenumber = $_POST['phoneNumber'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$cpassword = $_POST['cpassword'];
 
-//register user//
-$username = mysqli_real_escape_string($database,$_POST['firstName']);
-$phonenum = mysqli_real_escape_string($database,$_POST['phoneNumber']);
-$email = mysqli_real_escape_string($database,$_POST['email']);
-$password = mysqli_real_escape_string($database,$_POST['password']);
-$cpassword = mysqli_real_escape_string($database,$_POSTp['cpassword']);
-
-//validate infor//
+//if empty infornmation//
 if (empty($username)) {array_push($error,"Username is required.")};
 if (empty($phonenum)) {array_push($error,"Phone number is required.")};
 if (empty($email)) {array_push($error,"Email is required.")};
 if (empty($password)) {
     array_push($error,"Please fill your password.");
     if ($password != $cpassword){array_push($error, "Comfirm password does not match.")};
+} elseif ($password == $cpassword) {
+    $secur_passw = password_hash($password & $cpassword , $PASSWORD_BCRYPT)
 }
 
-
-//check database for existing user with same username//
-$user_check = "SELECT * FROM usersave WHERE phoneNumber = '$phonenum' or email = '$email' LIMIT 1";
-$results = mysqli_check($database,$user_check);
-$user = mysqli_fetch($results);
-if ($user) {
-    if ($user['phoneNunmber'] === $phonenum) {array_push($error,"Phonenumber already exist.");}
-    if ($user['email'] === $email) {array_push($error,"Email already exist.");}
-}
-
-//resgister if no error//
-if (count($error)==0) {
-    $password = password_hash($password,$PASSWORD_BCRYPT);;
-    $querry = "INSERT INTO usersave ('phoneNumber', 'email', 'password', 'firstName') VALUES ('$phonenum','$email','$password','$username')";
-    mysqli_check ($database,$query);
-    $_SESSION ['phoneNumber'] = $phonenum;
-    $_SESSION ['success'] = "You are now logged in.";
-    header ('location: MyAccount-Logged.php');
-}
-
-//login information//
-if (isset($_POST['login_user'])) {
-    $email = mysqli_real_escape_string($database,$_POST['email']);
-    $password = mysqli_real_escape_string($database,$_POST['password']);
-
-    if (empty($email)) {
-        array_push($error,"Email is required.");
+//if no error//
+if (count($error) == 0) {
+    $file_open = fopen("userinfor.csv","a");
+    $no_row = count(file("userinfor.csv"));
+    if ($no_row > 1) {
+        $no_row = ($no_row - 1) + 1;
     }
-    if (empty($password)) {
-        array_push($error,"Password is required.")
-    }
-    if (count($error) == 0) {
-        $password = password_hash($password, $PASSWORD_BCRYPT);
-        $querry = "SELECT * FROM usersave WHERE email = '$email' or password = '$password' ";
-        $results = mysqli_check($database,$querry);
-        if (mysqli_num_row($results)) {
-            $_SESSION['password'] = $password;
-            $_SESSION['success'] = "Logged successfully. Enjoy!";
-            header('location: MyAccount-Logged.php');
-        } else {
-            array_push($error, "Wrong email or password. Please try agian!")
-        }
-        $file_open = fopen("userinfor.csv", "a", $FILE_APPENDFILE, $LOCK_EX");
-        $no_row = count (file("userinfor.csv"));
-        if ($no_row > 1) { 
-            $no_row = ($no_row - 1) + 1;
-        }
-        $form_data = array (
-            'id' => $no_row,
-            'firstName' => $username,
-            'email' => $email,
-            'phoneNumber' => $phonenum
-        );
-        fputcsv($file_open, $form_data);
-        $no_row = '';
-        $username = '';
-        $email = '';
-        $phonenum = '';
-    }
+    $form_data = array(
+        'id' => $no_row,
+        'phonenum' => $phonenumber,
+        'name' => $username,
+        'email' => $email,
+        'password' => $password
+    );
+    fputcsv($file_open,$form_data);
+    $phonenumber= '';
+    $email='';
+    $username='';
+    $password='';
 }
-
-// //save infor into a file(userInforsave.txt)//
-// $myfile = 'userInforsave.txt';
-// $file = fopen($myfile,"w+");
-// file_put_contents("$file,$querry");
-?>

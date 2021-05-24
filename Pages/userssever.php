9d7bf161ac16666e035afe1454502c2e65018b76
Tran Nguyen Anh Khoa <s3863956@rmit.edu.vn>
2021-05-24 22:48:08 +0700

    reupload Kent's file

--- Pages/userssever.php
+++ Pages/userssever.php
@@ -16,15 +16,6 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $cpassword = $_POST['cpassword'];
 
-<<<<<<< HEAD
-//check database for existing user with same username//                 
-$user_check = "SELECT * FROM usersave WHERE PhoneNumber = '$phonenum' or Email = '$email' LIMIT 1";
-$results = mysqli_check($database,$user_check);
-$user = mysqli_fetch($results);
-if ($user) {
-    if ($user['PhoneNunmber'] === $phonenum) {array_push($error,"Phonenumber already exist");}
-    if ($user['Email'] === $email) {array_push($error,"Email already exist");}
-=======
 //if empty infornmation//
 if (empty($username)) {array_push($error,"Username is required.")};
 if (empty($phonenum)) {array_push($error,"Phone number is required.")};
@@ -34,7 +25,6 @@ if (empty($password)) {
     if ($password != $cpassword){array_push($error, "Comfirm password does not match.")};
 } elseif ($password == $cpassword) {
     $secur_passw = password_hash($password & $cpassword , $PASSWORD_BCRYPT)
->>>>>>> ef4214d834f302bd780777d0db03538345477273
 }
 
 //if no error//

977852009c948cb004e6b82e45cc801a8dc0f605
Your Name <s3803566@rmit.edu.vn>
2021-05-31 15:23:24 +0700

    noted and complete the verication for product page

--- Pages/MemberVeri.php
+++ Pages/MemberVeri.php
@@ -1,10 +1,16 @@
 <?php
 session_start();
 
+//Testing code to see if the pages turn
+//$_SESSION["loggedIn"] =TRUE;
+
+//Condition to see if users are logged in or not
 if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true)
 {
+    //Will be directed to order page if logged in
     $accessLink="OrderPage.php";
 
 }else{
+    //Will be directed to register page if not yet logged in
     $accessLink="Register.php";
 };
